218f5119585088ba3037168b8ea6d2811b2a0d48
Anthony Benedict <ashyantony7@gmail.com>
2020-04-22 02:01:08 +0200

    removed comments

--- Week_3/Leftmost_Column_with_at_Least_a_One.go
+++ Week_3/Leftmost_Column_with_at_Least_a_One.go
@@ -8,25 +8,25 @@
  */
 package main
 
-// func leftMostColumnWithOne(binaryMatrix BinaryMatrix) int {
+func leftMostColumnWithOne(binaryMatrix BinaryMatrix) int {
 
-// 	dim := binaryMatrix.Dimensions()
-// 	row, col := 0, dim[1]-1
-// 	var set bool
+	dim := binaryMatrix.Dimensions()
+	row, col := 0, dim[1]-1
+	var set bool
 
-// 	for (row < dim[0]) && (col >= 0) {
-// 		if binaryMatrix.Get(row, col) == 1 {
-// 			set = true
-// 			col--
-// 		} else {
-// 			row++
-// 		}
-// 	}
+	for (row < dim[0]) && (col >= 0) {
+		if binaryMatrix.Get(row, col) == 1 {
+			set = true
+			col--
+		} else {
+			row++
+		}
+	}
 
-// 	if set {
-// 		return col + 1
-// 	} else {
-// 		return -1
-// 	}
+	if set {
+		return col + 1
+	} else {
+		return -1
+	}
 
-// }
+}
